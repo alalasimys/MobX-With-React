5eb408f1ff63948f3c0b8d655fc0132915cebd8c
amoisieieva <a.moisieieva@andersenlab.com>
2024-03-10 20:07:16 +0200

    Add mobx helper hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { observer } from 'mobx-react-lite';
 import './App.css';
 import { Test1 } from './test';
+import { useStores } from './stores/helpers/use-stores';
 
 function App() {
+  const store = useStores();
+
   return (
     <div className="App">
       <Test1 />
+      {store.dataStores.usersStore.users.map((user) => (
+        <div>{user}</div>
+      ))}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { StoreProvider } from './stores/helpers/store-context';
+import { createStore } from './stores/helpers/create-store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootStore = createStore();
+
 root.render(
-  <React.StrictMode>
+  <StoreProvider value={rootStore}>
     <App />
-  </React.StrictMode>
+  </StoreProvider>
 );
